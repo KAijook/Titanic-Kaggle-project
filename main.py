2d23679611c677a51e6134c539156f319380e807
KAijook <162521172+KAijook@users.noreply.github.com>
2025-09-07 16:52:25 +0700

    Fix missing plt.show() for plot display
    Added missing plt.show() to display the plot.

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 import pandas as pd
-# Step 1: Collecting data
+#  Collecting data
 train_df = pd.read_csv(r"train.csv")
 test_df = pd.read_csv(r"test.csv")
 
-# Step 2: Preprocessing data by features engineering
+#  Preprocessing data by features engineering
 features = ["Sex", "Pclass", "Age", "SibSp", "Parch", "Fare", "Embarked"]
 
 
@@ -17,7 +17,7 @@ def preprocessing(df):
     return df
 
 
-# step 3: Start training models
+#  Training models
 train_df = preprocessing(train_df)
 test_df = preprocessing(test_df)
 X = train_df[features]
@@ -26,7 +26,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_
 model = RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
-# Step 4: Evaluate accuracy
+#  Evaluate accuracy
 print("Accuracy:", accuracy_score(y_test, y_pred))
 test_feature = test_df[features]
 test_pred = model.predict(test_feature)
@@ -34,7 +34,7 @@ Submission = pd.DataFrame({
     "PassengerId": test_df["PassengerId"],
     "Survived": test_pred
 })
-
+#  Showing graph
 importances = model.feature_importances_
 feat_imp = pd.DataFrame({"Feature": features, "Importance": importances})
 feat_imp = feat_imp.sort_values(by="Importance", ascending=False)
@@ -45,4 +45,5 @@ plt.xticks(rotation=45)
 plt.title("Feature Importance - Titanic Survival", fontsize=14)
 plt.xlabel("Đặc trưng")
 plt.ylabel("Độ quan trọng")
+
 plt.show()
